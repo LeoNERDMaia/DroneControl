4ceba386617a51e6c729c02a4a78060f703be420
Leonardo Geyer Maia <leogmaia@gmail.com>
2016-04-30 20:59:50 -0300

    Liftoff
    First scene and liftoff scrpit

---
+++ Assets/Script/BasicControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicControl : MonoBehaviour {
+
+    public float MotorForce = 1;
+    public Transform[] Motors;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach (Transform motor in Motors)
+        {
+            float TotalForce = Mathf.Clamp(Input.GetAxis("Throttle"), 0, 1) * (MotorForce / 10);
+            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(transform.up * TotalForce, motor.position, ForceMode.Impulse);
+        }
+	}
+}
